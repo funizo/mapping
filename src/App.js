67acab488aedd3bd9d3903e6f8572efe8e676a31
funizo <k4160621@naver.com>
2023-11-02 22:13:47 +0900

    Api공부중

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import './App.css';
 import Map from './map.js';
+import React , {useEffect, useState} from "react";
+const {kakao} = window;
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <Map/>
-    </div>
-  );
+
+  const [map,setMap] = useState(null);
+  const [level ,setLevel] = useState(3);
+
+  useEffect(()=> {
+        const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+        const options = { //지도를 생성할 때 필요한 기본 옵션
+	    center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+	    level: 3 //지도의 레벨(확대, 축소 정도)
+    };
+    const mapInstance = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+    setMap(mapInstance);
+    
+    },[level])
+
+    var marker = new kakao.maps.Marker({
+        map: map, 
+        position: new kakao.maps.LatLng(33.450701, 126.570667)
+    });
+
+    return(
+        <div id="map" style={{width:"1000px",height:"1000px"}}>
+            
+        </div>
+
+    )
+    
 }
 
 export default App;
